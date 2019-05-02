836459db451666d20a2c244e227f142bfb880a55
Andrew Byrne <anbyrne@microsoft.com>
2019-05-02 17:14:35 +0100

    split setup script

--- setup-sql-database.sh
+++ setup-sql-database.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 RESOURCE_GROUP=$(az group list --query "[0].name" -o tsv)
-TA_ACCT_NAME="TweetAnalytics"
-TA_LOCATION=$(shuf -n1 -e $(az cognitiveservices account list-skus --query "[?tier=='Free' && kind=='TextAnalytics'].locations[]" | jq -r '.[]' | sed '/EUAP$/d'))
-echo "Creating Text Analytics account..."
-az cognitiveservices account create --name "$TA_ACCT_NAME" --resource-group "$RESOURCE_GROUP" --kind TextAnalytics --sku F0 --location "$TA_LOCATION" --yes
-TA_KEY=$(az cognitiveservices account keys list --name $TA_ACCT_NAME --resource-group "$RESOURCE_GROUP" --query key1 -o tsv)
-TA_ENDPOINT=$(az cognitiveservices account show --name $TA_ACCT_NAME --resource-group "$RESOURCE_GROUP" --query endpoint -o tsv)
 SQL_LOCATION=$(az group show --name "$RESOURCE_GROUP" --query location -o tsv)
 SQL_LOCATION=WESTUS
 SQL_SERVERNAME="mslearn-exercise-sqlserver-$(openssl rand -hex 5)"
@@ -22,10 +16,6 @@ echo
 echo "Done!"
 echo
 echo "Make a note of the following values for use in the rest of this exercise:"
-echo
-echo "Cognitive Services account key: $TA_KEY"
-echo
-echo "Cognitive Services account endpoint: $TA_ENDPOINT"
 echo
 echo "SQL Server name: $SQL_SERVERNAME"
 echo
---
+++ setup-textanalytics.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+RESOURCE_GROUP=$(az group list --query "[0].name" -o tsv)
+TA_ACCT_NAME="TweetAnalytics"
+TA_LOCATION=$(shuf -n1 -e $(az cognitiveservices account list-skus --query "[?tier=='Free' && kind=='TextAnalytics'].locations[]" | jq -r '.[]' | sed '/EUAP$/d'))
+echo "Creating Text Analytics account..."
+az cognitiveservices account create --name "$TA_ACCT_NAME" --resource-group "$RESOURCE_GROUP" --kind TextAnalytics --sku F0 --location "$TA_LOCATION" --yes
+TA_KEY=$(az cognitiveservices account keys list --name $TA_ACCT_NAME --resource-group "$RESOURCE_GROUP" --query key1 -o tsv)
+TA_ENDPOINT=$(az cognitiveservices account show --name $TA_ACCT_NAME --resource-group "$RESOURCE_GROUP" --query endpoint -o tsv)
+echo "Done!"
+echo
+echo "Make a note of the following values for use in the rest of this exercise:"
+echo
+echo "Cognitive Services account key: $TA_KEY"
+echo
+echo "Cognitive Services account endpoint: $TA_ENDPOINT"
+echo
